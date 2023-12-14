fedc20cc9267390b146e28a98ae88d0a3f6873be
Felix Fischer <fe.fischer@icloud.com>
2023-12-14 18:01:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+data-similarity
+=======
+Data similarity.
+
+Licence
+-------
+MIT license
+
+Authors
+-------
+`scTab` was written by `Felix Fischer <felix.fischer@helmholtz-munich.de>`
+
+Support for software development, testing, modeling, and benchmarking provided by the Cell Annotation Platform team 
+(Roman Mukhin, Andrey Isaev, Uğur Bayındır)
---
+++ setup.py
@@ -0,0 +1,35 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type("")
+    with io.open(filename, mode="r", encoding="utf-8") as fd:
+        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
+
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="datasim",
+    version="0.1.0",
+    url="https://github.com/cellannotation/dataset-similarity",
+    license="MIT",
+    author="Felix Fischer",
+    author_email="felix.fischer@helmholtz-muenchen.de",
+    long_description=read("README.md"),
+    packages=find_packages(exclude=("tests",)),
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
