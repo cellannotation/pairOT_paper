30354c140ab11bc35ebaf4f0f6c57a6741c9d8e1
Felix Fischer <fe.fischer@icloud.com>
2025-03-04 15:49:10 +0100

    updated DEG filtering

--- datasim/de_testing/selection.py
+++ datasim/de_testing/selection.py
@@ -1,66 +1,47 @@
 from os.path import dirname, join
-from typing import Dict, Literal, Union
+from typing import Dict
 
 import pandas as pd
 
 from datasim.utils import _calc_scaled_jaccard
 
-GENE_FILTERING = {
-    "standard": join(dirname(__file__), "resources/filtered-genes.csv"),
-    "strict-pegasus-immune": join(
-        dirname(__file__), "resources/filtered-genes-strict-pegasus-immune.csv"
-    ),
-    "strict-villani-immune": join(
-        dirname(__file__), "resources/filtered-genes-strict-villani-immune.csv"
-    ),
-    "strict-villani-bonemarrow": join(
-        dirname(__file__), "resources/filtered-genes-strict-villani-bonemarrow.csv"
-    ),
-}
 
-
-def _filter_de_genes(
-    de_res: Dict[str, pd.DataFrame],
-    gene_filtering: Literal[
-        "standard",
-        "strict-pegasus-immune",
-        "strict-villani-immune",
-        "strict-villani-bonemarrow",
-        None,
-    ] = "standard",
-):
+def _filter_de_genes(de_res: Dict[str, pd.DataFrame]):
     de_res_return = {}
     # subset DE results to relevant genes
-    gene_names_filtered = pd.read_csv(GENE_FILTERING[gene_filtering])["feature_name"]
+    genes_to_filter = pd.read_csv(
+        join(dirname(__file__), "resources/filtered-genes.csv")
+    )["feature_name"].tolist()
+    official_gene_names = pd.read_csv(
+        join(dirname(__file__), "resources/official-genes.csv")
+    )["feature_name"].tolist()
     for ct, de_df in de_res.items():
-        gene_names = [elem for elem in gene_names_filtered if elem in de_df.index]
-        de_res_return[ct] = de_df[de_df.index.isin(gene_names)].copy()
+        # subset only to genes whose symbol is present on genenames.org
+        de_df = de_df[de_df.index.isin(official_gene_names)]
+        # filter out uninformative genes
+        de_df = de_df[~de_df.index.isin(genes_to_filter)]
+        de_res_return[ct] = de_df.copy()
 
     return de_res_return
 
 
 def sort_and_filter_de_genes_ova(
     de_res: Dict[str, pd.DataFrame],
+    logfc_threshold: float = 1.0,
     aucroc_threshold: float = 0.6,
     adj_pval_threshold: float = 0.05,
-    gene_filtering: Literal[
-        "standard",
-        "strict-pegasus-immune",
-        "strict-villani-immune",
-        "strict-villani-bonemarrow",
-        None,
-    ] = "standard",
+    gene_filtering: bool = True,
 ) -> Dict[str, pd.DataFrame]:
     """Select top-`n_genes` differentially expressed genes from the DE results."""
     # Subset DE results to relevant genes
-    if gene_filtering is not None:
-        de_res = _filter_de_genes(de_res, gene_filtering)
+    if gene_filtering:
+        de_res = _filter_de_genes(de_res)
     # Sort and filter DE genes
     top_de_genes = {}
     for ct, de_df in de_res.items():
         top_de_genes[ct] = (
             de_df.sort_values("logFC", ascending=False)
-            .query(f"`logFC` >= 1.")
+            .query(f"`logFC` >= {logfc_threshold}")
             .query(f"`adj.P.Val` <= {adj_pval_threshold}")
             .query(f"`auroc` >= {aucroc_threshold}")
             .copy()
@@ -71,30 +52,25 @@ def sort_and_filter_de_genes_ova(
 
 def sort_and_filter_de_genes_ava(
     de_res: Dict[str, Dict[str, pd.DataFrame]],
+    logfc_threshold: float = 1.0,
     aucroc_threshold: float = 0.6,
     adj_pval_threshold: float = 0.05,
-    gene_filtering: Literal[
-        "standard",
-        "strict-pegasus-immune",
-        "strict-villani-immune",
-        "strict-villani-bonemarrow",
-        None,
-    ] = "standard",
+    gene_filtering: bool = True,
 ):
     clusters = list(sorted(de_res.keys()))
     top_de_genes_ava = {ct: {} for ct in clusters}
     for ct1 in clusters:
         res = de_res[ct1]
         # Subset DE results to relevant genes
-        if gene_filtering is not None:
-            res = _filter_de_genes(res, gene_filtering)
+        if gene_filtering:
+            res = _filter_de_genes(res)
         # Sort and filter DE genes
         for ct2 in clusters:
             if ct1 != ct2:
                 top_de_genes_ava[ct1][ct2] = (
                     res[ct2]
                     .sort_values("logFC", ascending=False)
-                    .query(f"`logFC` >= 1.")
+                    .query(f"`logFC` >= {logfc_threshold}")
                     .query(f"`adj.P.Val` <= {adj_pval_threshold}")
                     .query(f"`auroc` >= {aucroc_threshold}")
                     .copy()
@@ -106,9 +82,10 @@ def sort_and_filter_de_genes_ava(
 def select_and_combine_de_results(
     de_res_ova: Dict[str, pd.DataFrame],
     de_res_ava: Dict[str, Dict[str, pd.DataFrame]],
-    n_genes_ova: Union[int, None] = 10,
-    n_genes_ava: Union[int, None] = 3,
-    overlap_threshold: float = 0.1,
+    n_genes_ova: int | None = 10,
+    n_genes_ava: int | None = 3,
+    n_genes_max: int | None = None,
+    overlap_threshold: float = 0.3,
     overlap_n_genes: int = 10,
     remove_duplicated_genes: bool = True,
 ) -> Dict[str, pd.DataFrame]:
@@ -143,6 +120,9 @@ def select_and_combine_de_results(
             reference = res["reference"].groupby(res.index).apply(lambda x: list(x))
             res = res[~res.index.duplicated(keep="first")]
             res["reference"] = reference
+        if n_genes_max is not None:
+            res = res.head(n_genes_max)
+
         combined_de_results[ct] = res.copy()
 
     return combined_de_results
